9c0a920ba0a150af95f85e57954918d479309a92
so0sora <latt03200909@gmail.com>
2025-07-04 11:16:09 +0900

    Update Scene.h

--- include/engine/Scene.h
+++ include/engine/Scene.h
@@ -4,9 +4,9 @@
 class Scene {
 public:
     virtual ~Scene() = default;
-    virtual void onEnter() = 0;      // ??ì§„ì… ???¸ì¶œ
-    virtual void onExit() = 0;       // ??ì¢…ë£Œ ???¸ì¶œ
-    virtual void handleEvent(const SDL_Event& e) = 0; // ?…ë ¥ ì²˜ë¦¬
-    virtual void update(float deltaTime) = 0;         // ?¼ë¦¬ ?…ë°?´íŠ¸
-    virtual void render(SDL_Renderer* renderer) = 0;  // ?Œë”ë§?
+    virtual void onEnter() = 0; 
+    virtual void onExit() = 0;
+    virtual void handleEvent(const SDL_Event& e) = 0; 
+    virtual void update(float deltaTime) = 0;
+    virtual void render(SDL_Renderer* renderer) = 0;
 };
